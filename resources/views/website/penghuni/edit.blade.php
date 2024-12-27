061d0fa7e8cd33e262b92a60bfaec339b7bb9ae2
ikadaniati26 <ikadaniati26@gmail.com>
2024-12-27 19:28:51 +0700

    27/12/2024 perbaikan show penghuni

--- resources/views/website/penghuni/edit.blade.php
+++ resources/views/website/penghuni/edit.blade.php
@@ -13,13 +13,13 @@
     <div class="row">
         <div class="col-xl-12">
         <div class="card p-4">
-            <form action="" method="POST">
+            <form action="{{ route('update_penghuni' , ['id' => $editpenghuni->idpenghuni])}}" method="POST">
                 @csrf
                 @method('PATCH')
                 <div class="row mb-3">
                     <label for="fullName" class="col-md-4 col-lg-3 col-form-label">Full Name</label>
                     <div class="col-md-8 col-lg-9">
-                        <input name="fullName" type="text" class="form-control" id="fullName"
+                        <input name="nama" type="text" class="form-control" id="fullName"
                             value="{{isset($editpenghuni->nama)?$editpenghuni->nama : ''}}">
                     </div>
                 </div>
@@ -39,7 +39,7 @@
                 <div class="row mb-3">
                     <label for="about" class="col-md-4 col-lg-3 col-form-label">Status Penghuni</label>
                     <div class="col-md-8 col-lg-9">
-                        <input name="fullName" type="text" class="form-control" id="fullName"
+                        <input name="status_penghuni" type="text" class="form-control" id="fullName"
                             value="{{isset($editpenghuni->status_penghuni)?$editpenghuni->status_penghuni: ''}}">
                     </div>
                 </div>
@@ -47,7 +47,7 @@
                 <div class="row mb-3">
                     <label for="company" class="col-md-4 col-lg-3 col-form-label">No Telp</label>
                     <div class="col-md-8 col-lg-9">
-                        <input name="company" type="text" class="form-control" id="company"
+                        <input name="no_telp" type="text" class="form-control" id="company"
                             value="{{isset($editpenghuni->no_telp)?$editpenghuni->no_telp: ''}}">
                     </div>
                 </div>
@@ -55,7 +55,7 @@
                 <div class="row mb-3">
                     <label for="Job" class="col-md-4 col-lg-3 col-form-label">Status Menikah</label>
                     <div class="col-md-8 col-lg-9">
-                        <input name="job" type="text" class="form-control" id="Job"
+                        <input name="status_menikah" type="text" class="form-control" id="Job"
                             value="{{isset($editpenghuni->status_menikah)?$editpenghuni->status_menikah: ''}}">
                     </div>
                 </div>
